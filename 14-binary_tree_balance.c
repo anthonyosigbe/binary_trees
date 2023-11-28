d25f4560eaa0cefc5ff6d6030473d20548b62ff1
Anthony Osigbe <thronesofglory@gmail.com>
2023-11-28 08:34:43 -0800

    14. Balance factor tasks completed

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,14 +8,12 @@
  * Return: If tree is NULL, returns 0;
  * otherwise, returns the balance factor.
  */
-
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-	return ((int)(binary_tree_height(tree->left)
-			- binary_tree_height(tree->right)));
+	return (0);
 }
 
 /**
@@ -26,14 +24,15 @@ int binary_tree_balance(const binary_tree_t *tree)
  * Return: If tree is NULL, the function returns 0;
  * otherwise, it returns the height.
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (!tree)
-		return (0);
+	if (tree)
+	{
+		size_t l = 0, r = 0;
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
-
-	return (1 + (left_height > right_height ? left_height : right_height));
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
 }
