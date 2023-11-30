c3b2b5533233975a332835b7acd2185bda9bb4f6
Anthony Osigbe <thronesofglory@gmail.com>
2023-11-30 06:16:06 -0800

    40. Heap - Sort tasks completed

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,30 @@
+#include "binary_trees.h"
+
+/**
+ * heap_to_sorted_array - Converts a Binary Max Heap to a sorted array
+ * @heap: Pointer to the root node of the heap to convert
+ * @size: Address to store the size of the array
+ *
+ * Return: Pointer to the sorted array of integers (descending order),
+ * or NULL on failure
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+    int *sorted_array;
+    size_t i;
+
+    if (heap == NULL || size == NULL)
+        return (NULL);
+
+    *size = binary_tree_size(heap);
+    sorted_array = malloc(*size * sizeof(int));
+
+    if (sorted_array == NULL)
+        return (NULL);
+
+    for (i = 0; i < *size; i++) {
+        sorted_array[i] = heap_extract(&heap);
+    }
+
+    return (sorted_array);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -91,4 +91,9 @@ int binary_tree_is_heap(const binary_tree_t *tree);
 heap_t *heap_insert(heap_t **root, int value);
 size_t binary_tree_size(const binary_tree_t *tree);
 heap_t *array_to_heap(int *array, size_t size);
+size_t tree_height(const heap_t *tree);
+size_t tree_size_h(const binary_tree_t *tree);
+void _preorder(heap_t *tree, heap_t **node, size_t height);
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 #endif /* _BINARY_TREES_H_ */
