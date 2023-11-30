07ed31e9c5db832ec564f04dd4a9dfb0b01cd6d5
Anthony Osigbe <thronesofglory@gmail.com>
2023-11-30 06:09:03 -0800

    38. Heap - Array to Binary Heap tasks completed

---
+++ 132-array_to_heap.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_heap - Builds a Max Binary Heap tree from an array
+ * @array: Pointer to the first element of the array to be converted
+ * @size: Number of elements in the array
+ *
+ * Return: Pointer to the root node of the created Binary Heap,
+ * or NULL on failure
+ */
+heap_t *array_to_heap(int *array, size_t size)
+{
+	heap_t *root = NULL;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		if (heap_insert(&root, array[i]) == NULL)
+			return (NULL);
+	}
+	return (root);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -88,4 +88,7 @@ int is_max_heap(const binary_tree_t *tree);
 int is_complete(const binary_tree_t *tree, size_t index, size_t nodes, size_t count);
 size_t count_nodes(const binary_tree_t *tree);
 int binary_tree_is_heap(const binary_tree_t *tree);
+heap_t *heap_insert(heap_t **root, int value);
+size_t binary_tree_size(const binary_tree_t *tree);
+heap_t *array_to_heap(int *array, size_t size);
 #endif /* _BINARY_TREES_H_ */
